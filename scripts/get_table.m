896302f1bca329436cf6ac7a57fe3b1c78bde5fe
torgil01 <torgil.vangberg@gmail.com>
2018-05-04 16:07:46 +0200

    fixed bug in index causing all values to be the same

--- scripts/get_table.m
+++ scripts/get_table.m
@@ -14,7 +14,7 @@ files = findFiles(imgDir,'.*\_SPECT_scropWarped.nii.gz');
 
 % read atlas labels
 % labels is a struct 2 x n struct 
-% containing label idex, and label name
+% containing label index, and label name
 labels = read_atlas_labels(atlasType,labelsPath);
 
 nLabels = length(labels{1});
@@ -32,7 +32,7 @@ for i = 1:length(files),
     [~,img] = readNii(files{i});
     fprintf('read %s\n',files{i});
     for j = 1:length(labels{1}),
-        labelNumber = labels{1}(i);
+        labelNumber = labels{1}(j);
         mask = bsxfun(@eq,atlas,labelNumber);        
         tmp = mask.*img;
         nonzero = tmp(tmp > 0);
@@ -43,8 +43,12 @@ end
 tableFile = 'table.csv';
 labs = {'Filename',labels{2}{:}};
 % construct table 
-vals = cell(nFiles,nLabels+1);
-vals(:,1) = files;   
+vals = cell(nFiles,nLabels+1); % extra filename,id, group, scan#
+vals(:,1) = files;
+% extract id, group and scan# from filename and put in array 
+%[~,fname] = rmExt(fpfn)
+
+
 vals(:,2:end)=num2cell(tableArray);
 T = cell2table(vals,'VariableNames',labs);
 
@@ -52,10 +56,7 @@ T = cell2table(vals,'VariableNames',labs);
 writetable(T,tableFile);
 
 
-function labels = read_atlas_labels(atlasType,labelsPath)
-fid = fopen(labelsPath);
-labels = textscan(fid,'%n %s','MultipleDelimsAsOne',1,'CommentStyle','//');
-fclose(fid);
+
 
 
 
