40ee442453547ec0d7dc911e52045f435ff5b628
torgil01 <torgil.vangberg@gmail.com>
2018-07-30 14:27:15 +0200

    Add parsing of WMH atlas labels

--- scripts/private/read_atlas_labels.m
+++ scripts/private/read_atlas_labels.m
@@ -1,5 +1,14 @@
 function labels = read_atlas_labels(atlasType,labelsPath)
 fid = fopen(labelsPath);
-labels = textscan(fid,'%n %s','MultipleDelimsAsOne',1,'CommentStyle','//');
+switch atlasType,
+    case 'Schwarz',        
+        labels = textscan(fid,'%n %s','MultipleDelimsAsOne',1,'CommentStyle','//');
+    case 'WHS',        
+        labels = textscan(fid,'%n %*d %*d %*d %*d %*d %*d %q','MultipleDelimsAsOne',1,'CommentStyle','#');
+    otherwise,
+        fclose(fid);
+        error('Unknown atlas type')
+end
 fclose(fid);
+
 
