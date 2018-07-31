cfa7a01d5578b89efbd6ee9cf7904b439b7a3925
torgil01 <torgil.vangberg@gmail.com>
2018-07-31 16:24:44 +0200

    first commit

---
+++ scripts/resample_whs_atlas.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Resample WHS atlas to 0.2 mm iso using ants:ResampleImageBySpacing
+
+atlasDir=/home/torgil/tmp/rotte/atlas/whs/
+
+# use nearest neighbor interpolation for these files
+nnFiles=(WHS_rat_atlas_hemi.nii.gz \
+	 WHS_SD_v2_brainmask_bin.nii.gz \
+	 WHS_SD_v2_white_gray_mask_clipped.nii.gz)
+
+# trilinear for this
+intFiles=(WHS_SD_rat_T2star_v1.01.nii.gz)
+
+
+for f in ${nnFiles[@]}; do
+    fn=$(remove_ext $f)
+    inputFile=${atlasDir}/${f}
+    outputFile=${atlasDir}/${fn}"_02mm.nii.gz"
+    ResampleImageBySpacing 3 $inputFile $outputFile 0.2 0.2 0.2 0 0 1 
+done
+
+for f in ${intFiles[@]}; do
+    fn=$(remove_ext $f)
+    inputFile=${atlasDir}/${f}
+    outputFile=${atlasDir}/${fn}"_02mm.nii.gz"
+    ResampleImageBySpacing 3 $inputFile $outputFile 0.2 0.2 0.2 0 0 0
+done
+
+
