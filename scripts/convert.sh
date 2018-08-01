15a1fe93a87255ecce44959222747fe442d20fb7
torgil01 <torgil.vangberg@gmail.com>
2018-08-01 14:05:20 +0200

    added getops and option for using the WHS or Schwarz atlases

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,16 +1,56 @@
 #!/bin/bash
 
-dcmDir=$1
-niiDir=$2
+# parse args
+while getopts "hd:n:t:" flag
+do
+  case "$flag" in
+      d)
+	  # t1w input volume
+	  dcmDir=$(readlink -f  "$OPTARG")
+	  ;;
+      n)
+	  # id for current input
+	  niiDir=$(readlink -f  "$OPTARG")
+	  ;;
+      t) 
+	  # destination dir for FS output
+	  target=${OPTARG,,} # convert to lowercase
+	  case  "$target" in
+	      whs)
+		  ox=43
+		  oy=-45
+		  oz=-51		  
+		  ;;
+	      schwarz)
+		  ox=12
+		  oy=-9
+		  oz=-16
+		  ;;
+	      ?)
+	      echo "Unknown target $target, exiting"
+	      exit 2
+	      ;;
+	  esac	  
+	  ;;
+      h)
+	  echo "Usage: $0 -d <dicom-dir> -n <nii-dir> -t <Schwarz | whs> "
+	  exit 0
+	  ;;
+      ?)    
+      echo Unknown input flag $flag
+      echo "Usage: $0 -d <dicom-dir> -n <nii-dir> -t <Schwarz | whs> "
+      exit 2
+      ;;
+  esac
+done
+
 
 dcmFiles=$(find $dcmDir -type f -name \*)
 
 for file in ${dcmFiles[@]}; do
     echo "$file"
     filename=$(basename $file)
-    #echo $filename
-    # baseDir=$(dirname $file)
-    
+    # baseDir=$(dirname $file)    
     dcm2niix -f ${filename} -s y -o ${niiDir} $file 
     # fix orientation issue
     niiFile="${niiDir}/${filename}.nii.gz"
@@ -18,9 +58,9 @@ for file in ${dcmFiles[@]}; do
     fslorient -deleteorient $niiFile # delete orientation info
     fslswapdim $niiFile -x z y  $tmpFile # swap dims so that they are correct in fslview
     fslorient -setsformcode 1 $tmpFile # set correct header
-    # the following two line shifts the origin such that roughly it matches the Schwartz atlas
-    fslorient -setqform -0.177 0 0 12 0 0.177 0 -9 0 0 0.177 -16 0 0 0 1 $tmpFile
-    fslorient -setsform -0.177 0 0 12 0 0.177 0 -9 0 0 0.177 -16 0 0 0 1 $tmpFile
+    # the following two line shifts the origin such that roughly it matches the target atlas
+    fslorient -setqform -0.177 0 0 $ox 0 0.177 0 $oy 0 0 0.177 $oz 0 0 0 1 $tmpFile
+    fslorient -setsform -0.177 0 0 $ox 0 0.177 0 $oy 0 0 0.177 $oz 0 0 0 1 $tmpFile
     rm $niiFile
     mv $tmpFile $niiFile
 done
