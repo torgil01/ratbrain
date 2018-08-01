3fe8c59a0dee44b2a4d44cdc79718d0ed24384a4
torgil01 <torgil.vangberg@gmail.com>
2018-08-01 14:06:01 +0200

    added one extra file for resampling

--- scripts/resample_whs_atlas.sh
+++ scripts/resample_whs_atlas.sh
@@ -6,6 +6,7 @@ atlasDir=/home/torgil/tmp/rotte/atlas/whs/
 
 # use nearest neighbor interpolation for these files
 nnFiles=(WHS_rat_atlas_hemi.nii.gz \
+	 WHS_SD_rat_atlas_v2.nii.gz  \
 	 WHS_SD_v2_brainmask_bin.nii.gz \
 	 WHS_SD_v2_white_gray_mask_clipped.nii.gz)
 
