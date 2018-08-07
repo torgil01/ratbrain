b3a79cfcaf18c20adbc25992aa82d6d22af03d70
torgil01 <torgil.vangberg@gmail.com>
2018-08-07 10:47:15 +0200

    remove label indexing when construction diff matrix
    instead of adressing array indices we use labels to asess the correct
    data. This is safer.

--- analysis/scaleAndDiff.R
+++ analysis/scaleAndDiff.R
@@ -17,28 +17,19 @@ scaleAndDiff <- function(rawData){
 
   # get unique ids
   idList <- unique(scaledData$id)
-  
-  # allocate new dataframe for t2 - t1 diff
-  # we just use the original to get the labels
-  #diffValues <- scaledData[FALSE,]
-  #diffValues$sess <- NULL
-  #diffValues$Filename <- NULL
-  #diffValues$globMean <- NULL
-  #diffValues$brainMean <- NULL
-  
   numVar <- length(roiLabels)
   group <- vector()
   d <- matrix(nrow = length(idList), ncol=length(roiLabels))
   
   for (i in 1:length(idList)){
-    t1 <- as.matrix(scaledData[(scaledData$id == idList[i]) & (scaledData$sess == 1),5:(4+numVar)])
-    t2 <- as.matrix(scaledData[(scaledData$id == idList[i]) & (scaledData$sess == 2),5:(4+numVar)])
+   # t1 <- as.matrix(scaledData[(scaledData$id == idList[i]) & (scaledData$sess == 1),5:(4+numVar)])
+  #   t2 <- as.matrix(scaledData[(scaledData$id == idList[i]) & (scaledData$sess == 2),5:(4+numVar)])
+  t1 <- as.matrix(scaledData[(scaledData$id == idList[i]) & (scaledData$sess == 1),roiLabels])
+  t2 <- as.matrix(scaledData[(scaledData$id == idList[i]) & (scaledData$sess == 2),roiLabels])
+    
     grp <- scaledData[(scaledData$id == idList[i]) & (scaledData$sess == 1),]$group
     d[i,] <- t2 - t1
-    #diffValues[i,]$id <- idList[i]
     group[i] <- grp
-    #diffValues[i,]$group  <- grp
-    #diffValues[i,3:3+numVar] <- d
   }
   
   diffValues <- data.frame(idList,group,d)
